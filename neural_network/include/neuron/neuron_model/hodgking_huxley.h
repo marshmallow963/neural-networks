1f062153f32bf6fef27a7e67dc43e9d40ab40a60
marshmallow963 <alexrodrigues.ars963@gmail.com>
2025-07-25 18:37:40 -0300

    Hodgking-Huxley neuron interface

--- neural_network/include/neuron/neuron_model/hodgking_huxley.h
+++ neural_network/include/neuron/neuron_model/hodgking_huxley.h
@@ -1,36 +1,9 @@
 #ifndef HODGKING_HUXLEY_H
 #define HODGKING_HUXLEY_H
 
-typedef struct {
-  // Conductancy (Sodium, Potassium, Leak)
-  double gNa, gK, gL;
-  // Reversal potential (Sodium, Potassium, Leak)
-  double eNa, eK, eL;
-  // Membrane capacitancy
-  double cM;
-} HodgkingHuxleyParams;
-
-typedef struct {
-  // Ionic gates
-  double mGate, hGate, nGate;
-  // Voltage, External and Synaptic currents
-  double v, iExt, iSyn;
-  // ionic currents (Sodium, Potassium, Leak)
-  double iNa, ik, iL;
-} HodgkingHuxleyState;
-
-typedef struct {
-  HodgkingHuxleyParams *params;
-  HodgkingHuxleyState *state;
-} HodgkingHuxleyNeuron;
-
-// Ionic gates in steady state
-double M_inf(double voltage);
-double H_inf(double voltage);
-double N_inf(double voltage);
+#include "neuron_config/neuron_structure.h"
 
 // Initialization functions
-// The neuron is inicialized with its resting potential
 void InitHodgkingHuxleyParams(HodgkingHuxleyParams *params);
 void InitHodgkingHuxleyState(HodgkingHuxleyState *state);
 void InitHodgkingHuxleyNeuron(HodgkingHuxleyNeuron *neuron);
