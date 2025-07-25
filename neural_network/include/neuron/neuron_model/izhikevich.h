b9bae956b3fb6e2193e87e78d65e463233e2b5d4
marshmallow963 <alexrodrigues.ars963@gmail.com>
2025-07-25 18:47:39 -0300

    Izhikevich neuron interface

--- neural_network/include/neuron/neuron_model/izhikevich.h
+++ neural_network/include/neuron/neuron_model/izhikevich.h
@@ -1,37 +1,8 @@
 #ifndef IZHIKEVICH_H
 #define IZHIKEVICH_H
 
-typedef enum {
-  // Excitatory
-    IntrinsicallyBursting,
-    RegularSpiking,
-    FastSpiking,
-    // Inhibitory
-    LowThresholdSpiking,
-    ThalamoCortical
-} IzhikevichNeuronType;
+#define "neuron_config/neuron_structure.h"
 
-typedef struct {
-  double a; // Membrane potential recovery after spike 
-  double b; // Sensitivity of the recovery variable 'u'
-  double c; // Membrane potential reset value
-  double d; // Recovery variable reset value
-} IzhikevichNeuronParams;
-
-typedef struct {
-  double iExt; // External current
-  double iSyn; // Synaptic current
-  double v; // Membrane potentail
-  double u; // recovery variable
-} IzhikevichNeuronState;
-
-typedef struct {
-  IzhikevichNeuronParams *params;
-  IzhikevichNeuronState *state;
-  IzhikevichNeuronType type;
-} IzhikevichNeuron;
-
-// The neuron is inicialized with its resting potential
 void InitIzhikevichNeuronParams(IzhikevichNeuronParams *params, IzhikevichNeuronType type);
 void InitIzhikevichNeuronState(const IzhikevichNeuronParams *params, IzhikevichNeuronState *state, IzhikevichNeuronType type);
 void InitIzhikevichNeuron(IzhikevichNeuron *neuron, IzhikevichNeuronType type);
