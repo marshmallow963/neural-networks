ff297ce7b1a023d4a8378d79049174d99d5a752e
marshmallow963 <alexrodrigues.ars963@gmail.com>
2025-07-25 18:43:32 -0300

    Structure for neuron`s model

--- neural_network/include/neuron/neuron_model/neuron_config/neuron_structure.h
+++ neural_network/include/neuron/neuron_model/neuron_config/neuron_structure.h
@@ -4,50 +4,50 @@
 /* --- Neuron Structure for Hodgking-Huxley Neuron Model --- */
 
 typedef struct {
-  double gNa, gK, gL;
-  double eNa, eK, eL;
-  double mC;
+    double gNa, gK, gL;
+    double eNa, eK, eL;
+    double mC;
 } HodgkingHuxleyNeuronParams;
 
 typedef struct {
-  double mGate, hGate, nGate;
+	double mGate, hGate, nGate;
 	double v, iExt, iSyn;
-	double iNa, iK, iL;
+  	double iNa, iK, iL;
 } HodgkingHuxleyNeuronState;
 
 typedef struct {
-  HodgkingHuxleyParams *params;
+	HodgkingHuxleyParams *params;
 	HodgkingHuxleyState *state;
 } HodgkingHuxleyNeuron;
 
 /* --- Neuron Structure for Izhikevich Neuron Model --- */
 
 typedef enum {
-  // Neuron Excitatory
-  IntrinsicallyBursting,
-  RegularSpiking,
-  FastSpiking,
-  // Neuron Inhibitory
-  LowThresholdSpiking,
-  ThalamoCortical
+	// Neuron Excitatory
+	IntrinsicallyBursting,
+	RegularSpiking,
+	FastSpiking,
+	// Neuron Inhibitory
+	LowThresholdSpiking,
+	ThalamoCortical
 } IzhikevichNeuronType;
 
 typedef struct {
-  double a;
-  double b
-  double c; 
-  double d;
+	double a;
+	double b
+	double c; 
+	double d;
 } IzhikevichNeuronParams;
 
 typedef struct {
-    double iExt, iSyn;
-    double v, u;
+	double iExt, iSyn;
+	double v, u;
 } IzhikevichNeuronState;
 
 typedef struct {
-    IzhikevichParams *params;
-    IzhikevichState *state;
-    IzhikevichType type;
+	IzhikevichParams *params;
+	IzhikevichState *state;
+	IzhikevichType type;
 } IzhikevichNeuron;
 
 #endif // NEURON_STRUCTURE_H
