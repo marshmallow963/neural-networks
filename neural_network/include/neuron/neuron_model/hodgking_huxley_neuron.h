75345517e0deeec527fd6113d37671d30f672769
marshmallow963 <alexrodrigues.ars963@gmail.com>
2025-07-27 20:44:40 -0300

    Hodgking-Huxley neuron functions

--- neural_network/include/neuron/neuron_model/hodgking_huxley_neuron.h
+++ neural_network/include/neuron/neuron_model/hodgking_huxley_neuron.h
@@ -1,7 +1,7 @@
-#ifndef HODGKING_HUXLEY_H
-#define HODGKING_HUXLEY_H
+#ifndef HODGKING_HUXLEY_NEURON_H
+#define HODGKING_HUXLEY_NEURON_H
 
-#include "neuron_config/neuron_structure.h"
+#include "neuron_structure/hodgking_huxley_neuron_structure.h"
 
 // Initialization functions
 void InitHodgkingHuxleyParams(HodgkingHuxleyNeuronParams *params);
@@ -9,7 +9,7 @@ void InitHodgkingHuxleyState(HodgkingHuxleyNeuronState *state);
 void InitHodgkingHuxleyNeuron(HodgkingHuxleyNeuron *neuron);
 
 void HodgkingHuxleyequations(void *modelNeuron, const double *stateVetcor, double *derivative);
-void UpdateHodgkingHuxleyNeuron(HodgkingHuxleyNeuron *neuron, double *currents, double dt);
+void UpdateHodgkingHuxleyNeuron(HodgkingHuxleyNeuron *neuron, double I, double dt);
 
 void GetHodgkingHuxleyNeuronIonicCurrets(const HodgkingHuxleyNeuron *neuron, double *ionicCurrents);
 void GetHodgkingHuxleyNeuronIonicGates(const HodgkingHuxleyNeuron *neuron, double *gates);
@@ -17,4 +17,4 @@ void GetHodgkingHuxleyNeuronVoltage(const HodgkingHuxleyNeuron *neuron, double *
 
 void DestroyNeuron(HodgkingHuxleyNeuron *neuron);
 
-#endif // HODGKING_HUXLEY_H
+#endif // HODGKING_HUXLEY_NEURON_H
