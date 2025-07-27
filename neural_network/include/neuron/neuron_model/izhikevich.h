704f756beea4fba3f442b3fc5a968dc254472e94
marshmallow963 <alexrodrigues.ars963@gmail.com>
2025-07-27 20:45:56 -0300

    Izhikevich neuron functions

--- neural_network/include/neuron/neuron_model/izhikevich.h
+++ neural_network/include/neuron/neuron_model/izhikevich.h
@@ -1,14 +1,14 @@
 #ifndef IZHIKEVICH_H
 #define IZHIKEVICH_H
 
-#define "neuron_config/neuron_structure.h"
+#define "neuron_Structure/izhikevich_neuron_structure.h"
 
 void InitIzhikevichNeuronParams(IzhikevichNeuronParams *params, IzhikevichNeuronType type);
 void InitIzhikevichNeuronState(const IzhikevichNeuronParams *params, IzhikevichNeuronState *state, IzhikevichNeuronType type);
 void InitIzhikevichNeuron(IzhikevichNeuron *neuron, IzhikevichNeuronType type);
 
 void IzhikevichNeuronEquations(const double *stateVector, void *neuronModel, void *derivative);
-void UpdateIzhikevichNeuron(IzhikevichNeuron *neuron, double *currents, double dt);
+void UpdateIzhikevichNeuron(IzhikevichNeuron *neuron, double I, double dt);
 
 void GetIzhikevichneuronRecovery(const IzhikevichNeuron *neuron, double *recovery);
 void GetIzhikevichNeuronVoltage(const IzhikevichNeuron *neuron, double *voltage);
