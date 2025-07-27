c410367c17b0b809774968d4976a36f37e405744
marshmallow963 <alexrodrigues.ars963@gmail.com>
2025-07-27 20:41:24 -0300

    Structure for Hodgking-Huxley neuron model

--- neural_network/include/neuron/neuron_model/neuron_structure/hodgking_huxley_neuron_structure.h
+++ neural_network/include/neuron/neuron_model/neuron_structure/hodgking_huxley_neuron_structure.h
@@ -4,30 +4,28 @@
 /* --- Neuron Structure for Hodgking-Huxley Neuron Model --- */
 
 typedef struct {
-	double gNa;  // Conductancy for sodium current
-	double eNa;  // Equilibrium potential for sodium current
-	double gk;   // Conductancy for potassium current
-	double eK;   // Equilibrium potential for potassium current
-	double gL;   // Conductancy for leak current
-	double eL;   // Equiulibrium potential for leak current
-	double c;    // Membrane capacitancy
+    double gNa; // Conductancy for sodium current
+    double eNa; // Equilibrium potential for sodium current
+    double gk;  // Conductancy for potassium current
+    double eK;  // Equilibrium potential for potassium current
+    double gL;  // Conductancy for leak current
+    double eL;  // Equiulibrium potential for leak current
+    double c;   // Membrane capacitancy
 } HodgkingHuxleyNeuronParams;
 
 typedef struct {
-	double m;	 // Sodium activation gate variable
-	double h; 	 // Sodium inactivation gate variable
-	double n; 	 // Potassium activation gate variable
-	double iNa;  // Sodium current
-	double iK;	 // Potassium current
-	double iL;	 // Leak current
-	double iExt; // External current
-	double iSyn; // Synaptic current
-	double v;	 // Membrane potential
+    double m;   // Sodium activation gate variable
+    double h;   // Sodium inactivation gate variable
+    double n;   // Potassium activation gate variable
+    double iNa; // Sodium current
+    double iK;  // Potassium current
+    double iL;  // Leak current
+    double v;   // Membrane potential
 } HodgkingHuxleyNeuronState;
 
 typedef struct {
-	HodgkingHuxleyNeuronParams *params;
-	HodgkingHuxleyNeuronState  *state;
+    HodgkingHuxleyNeuronParams *params;
+    HodgkingHuxleyNeuronState  *state;
 } HodgkingHuxleyNeuron;
 
 #endif // HODGKING_HUXLEY_NEURON_STRUCTURE_H
