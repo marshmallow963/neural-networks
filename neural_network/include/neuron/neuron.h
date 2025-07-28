55c4a9e7d467e1f369305d26810916df245317b2
marshmallow963 <alexrodrigues.ars963@gmail.com>
2025-07-28 18:48:02 -0300

    Neuron functions

--- neural_network/include/neuron/neuron.h
+++ neural_network/include/neuron/neuron.h
@@ -4,11 +4,11 @@
 #include "neuron_model/hodgking_huxley_neuron.h"
 #include "neuron_model/izhikevich_neuron.h"
 
-#include "structure.h"
+#include "neuron_generic.h"
 
-void CreateNeuron(NeuronType type, Neuron *neuron, int id);
-void UpdateNeuron(Neuron *neuron, double I, double dt);
-void GetNeuronVoltage(const Neuron *neuron, double *voltage);
-void DestroyNeuron(Neuron *neuron);
+void CreateNeuron(NeuronType type, NeuronInterface *neuron, int id);
+void UpdateNeuron(Neuron *neuronInterface, double currentTotal, double dt);
+void GetNeuronVoltage(const NeuronInterface *neuron, double *voltage);
+void DestroyNeuron(NeuronInterface *neuron);
 
 #endif // NEURON_H
